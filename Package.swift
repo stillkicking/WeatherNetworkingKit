c668ce0979063614769aa6f515903626a746a6cf
Jonathan Saville <jonathan.saville@mac.com>
2023-10-19 13:43:02 +0100

    updated iOS platform version to .v13

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "WeatherNetworkingKit",
     platforms: [
-        .iOS(.v10),
+        .iOS(.v13),
     ],
     products: [
         .library(
@@ -17,7 +17,7 @@ let package = Package(
         .binaryTarget(
             name: "WeatherNetworkingKit",
             url: "https://github.com/still-kicking-test/WeatherNetworkingKit/releases/download/v1.0.0/WeatherNetworkingKit.xcframework.zip",
-            checksum: "4ef09bbf75574a941ae547e719f30b109c8bc7f72cdf79563ddf8d8b16c7141a"
+            checksum: "572f0e6cb6afc94600f93a0ffea47da46b77d7177ce0d87870fdbc85659869ee"
         ),
     ]
 )
