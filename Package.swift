8ae357543f0800761a17dad2ad0ed339803bc929
Jonathan Saville <jonathan.saville@mac.com>
2023-11-09 14:35:38 +0000

    update checksum for v1.3.1

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WeatherNetworkingKit",
-            url: "https://github.com/still-kicking-test/WeatherNetworkingKit/releases/download/v1.3.0/WeatherNetworkingKit-1.3.0.xcframework.zip",
-            checksum: "6e50514fdd78c9f2fa543a1fa6a9803c45978e68a4416caee9a26efc432cd61b"
+            url: "https://github.com/still-kicking-test/WeatherNetworkingKit/releases/download/v1.3.1/WeatherNetworkingKit-1.3.1.xcframework.zip",
+            checksum: "f4c009dafc6f75e71494e3e9937a309b4080d880913cf55804d4836d9d1671dd"
         ),
     ]
 )
