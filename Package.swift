b1f961e780fcb346d5291d33fb764fd1b10df5cc
Jonathan Saville <jonathan.saville@mac.com>
2023-11-21 11:00:43 +0000

    Add moonrise, moonset, moonPhase & summary to forecast response

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WeatherNetworkingKit",
-            url: "https://github.com/still-kicking-test/WeatherNetworkingKit/releases/download/v1.3.2/WeatherNetworkingKit-1.3.2.xcframework.zip",
-            checksum: "32f107e136740f912d39d471bba5170e6b6414c5df84da50a494c70680ac79bb"
+            url: "https://github.com/still-kicking-test/WeatherNetworkingKit/releases/download/v1.3.3/WeatherNetworkingKit-1.3.3.xcframework.zip",
+            checksum: "28953dc4751b72d5a61b920e8e521432a169552040e8905b4fafb1b17aab7968"
         ),
     ]
 )
