1effcad519e024dce705a2bd1d084f6d0c046654
Jonathan Saville <jonathan.saville@mac.com>
2024-03-15 10:25:33 +0000

    Added UUID to Forecast model

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WeatherNetworkingKit",
-            url: "https://github.com/still-kicking-test/WeatherNetworkingKit/releases/download/v1.4.0/WeatherNetworkingKit-1.4.0.xcframework.zip",
-            checksum: "3d520dc625c977f44da60f2b5e80917486e69637755c44cb58aae9b37750685c"
+            url: "https://github.com/still-kicking-test/WeatherNetworkingKit/releases/download/v1.4.1/WeatherNetworkingKit-1.4.1.xcframework.zip",
+            checksum: "8fe077c78b75a1b64162718b65b51c6d436ecc8e71b9f5e81ec2fa1bb3e1e69c"
         ),
     ]
 )
