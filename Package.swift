03224f14a16e5b0aad2867f12b030e29009e0f7d
Jonathan Saville <jonathan.saville@mac.com>
2024-03-21 08:44:10 +0000

    v1.4.2

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WeatherNetworkingKit",
-            url: "https://github.com/still-kicking-test/WeatherNetworkingKit/releases/download/v1.4.1/WeatherNetworkingKit-1.4.1.xcframework.zip",
-            checksum: "8fe077c78b75a1b64162718b65b51c6d436ecc8e71b9f5e81ec2fa1bb3e1e69c"
+            url: "https://github.com/still-kicking-test/WeatherNetworkingKit/releases/download/v1.4.2/WeatherNetworkingKit-1.4.2.xcframework.zip",
+            checksum: "6e9dcd89456ddc6b6d3282c234e9ddec94ffc969caeb152c0b0163df35422a27"
         ),
     ]
 )
