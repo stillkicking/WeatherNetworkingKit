6ec007d8cd41b5a2e0baff8fa616f65e605d8b7a
Jonathan Saville <jonathan.saville@mac.com>
2024-07-06 15:50:03 +0100

    Added asyncAwait enabled services

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WeatherNetworkingKit",
-            url: "https://github.com/still-kicking-test/WeatherNetworkingKit/releases/download/v1.4.2/WeatherNetworkingKit-1.4.2.xcframework.zip",
-            checksum: "6e9dcd89456ddc6b6d3282c234e9ddec94ffc969caeb152c0b0163df35422a27"
+            url: "https://github.com/still-kicking-test/WeatherNetworkingKit/releases/download/v1.5.0/WeatherNetworkingKit-1.5.0.xcframework.zip",
+            checksum: "01a9aa4c5fc2926e836468069e2f28c4aa426022b29caf6c65c0a8c0e00d524e"
         ),
     ]
 )
